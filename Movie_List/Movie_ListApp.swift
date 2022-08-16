829c260954434927f1ea7b2f695ab41507a84069
Habib Akbari <habib.wt.a@gmail.com>
2022-08-16 12:10:00 +0430

    Initial Commit

---
+++ Movie_List/Movie_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ListApp.swift
+//  Movie_List
+//
+//  Created by Habib Akbari on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
