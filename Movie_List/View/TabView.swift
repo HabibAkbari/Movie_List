ae1c07f6cd8e43588b3a717bd5148ab827538f92
Habib Akbari <habib.wt.a@gmail.com>
2022-08-16 13:15:07 +0430

    Add TabView.swift File

---
+++ Movie_List/View/TabView.swift
@@ -0,0 +1,20 @@
+//
+//  TabView.swift
+//  Movie_List
+//
+//  Created by Habib Akbari on 8/16/22.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text("Tab View")
+    }
+}
+
+struct TabView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabView()
+    }
+}
